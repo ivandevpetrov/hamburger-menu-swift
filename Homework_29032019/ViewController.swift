19b1c40d640970f95e1d5e602a0081351a2a5728
notayslo <antonsipaylo@gmail.com>
2019-03-29 12:27:20 +0300

    Initial Commit

---
+++ Homework_29032019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework_29032019
+//
+//  Created by Anton Sipaylo on 3/29/19.
+//  Copyright © 2019 Anton Sipaylo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
